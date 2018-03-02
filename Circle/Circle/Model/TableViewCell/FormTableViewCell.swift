8490c449de0a79b0b14dcdf2563a07241ac57c56
Leonardo Augusto Piovezan <leonardoapiovezan@gmail.com>
2018-03-02 18:12:21 -0300

    Evaluation form in process. Using a pod called Eureka.

---
+++ Circle/Circle/Model/TableViewCell/FormTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FormTableViewCell.swift
+//  Circle
+//
+//  Created by Leonardo Augusto Piovezan on 02/03/18.
+//  Copyright © 2018 Gabriel Branco Frizzo. All rights reserved.
+//
+
+import UIKit
+
+class FormTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
