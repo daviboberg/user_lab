de8ea32cc22a765fe1e61c4fd021c8da7f1ec316
Leonardo Augusto Piovezan <leonardoapiovezan@gmail.com>
2018-03-02 10:56:25 -0300

    Starting to adapt table view from user list to RxSwit

--- Circle/Circle/Controller/UserListViewController.swift
+++ Circle/Circle/Controller/UserListViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class UserListViewController: UIViewController {
 
@@ -20,16 +22,14 @@ class UserListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+}
+
+
+extension UserListViewController{
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func setUpBinding(){
+        
     }
-    */
-
 }
