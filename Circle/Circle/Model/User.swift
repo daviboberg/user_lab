43cbdf03b89e8cfda350a5e417e7bf8e58dd08da
Leonardo Augusto Piovezan <leonardoapiovezan@gmail.com>
2018-03-05 09:56:25 -0300

    Class user(archive) and small changes in storyboard

---
+++ Circle/Circle/Model/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  Circle
+//
+//  Created by Gabriel Branco Frizzo on 02/03/18.
+//  Copyright © 2018 Gabriel Branco Frizzo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class User {
+//    struct UserInfo: Codable{
+//        var username: String
+//        var email: String
+//        var team: String
+//        var function: String
+//        var image: UIImage?
+//    }
+    
+//    init(response: Any){
+//        let decoder = JSONDecoder()
+//        do{
+//            let info = try decoder.decode(<#T##type: Decodable.Protocol##Decodable.Protocol#>, from: <#T##Data#>)
+//        } catch {
+//
+//        }
+//
+//    }
+}
