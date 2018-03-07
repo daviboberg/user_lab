c504f6e5b90e53f34440f4418f64734b06c495e9
Giulia Padovani <gcesarpadovani@hotmail.com>
2018-03-07 08:53:58 -0300

    Add 1 parameter (pageSize), commented

--- Circle/Circle/Model/Network/UsersCollectRouter.swift
+++ Circle/Circle/Model/Network/UsersCollectRouter.swift
@@ -12,6 +12,12 @@ enum UsersCollectRouter {
     case getUsersCollect(token:String)
 }
 
+/*
+enum UsersCollectRouter {
+    case getUsersCollect(token:String, pageSize: Int)
+}
+*/
+
 extension UsersCollectRouter:TargetType {
     var baseURL: URL {
         return URL(string: "https://app.pingboard.com/api/v2")!
@@ -36,6 +42,16 @@ extension UsersCollectRouter:TargetType {
         return .requestPlain
     }
     
+    /*
+    var task: Task {
+        if let parameters = self.parameters {
+            return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+        } else {
+            return .requestPlain
+        }
+    }
+    */
+    
     var headers: [String : String]? {
         switch self {
         case .getUsersCollect(let token):
@@ -43,5 +59,18 @@ extension UsersCollectRouter:TargetType {
             
         }
     }
+    
+    /*
+    var parameters: [String : Any]? {
+        switch self {
+        case .getUsersCollect(_,let pageSize):
+            return ["pageSize": "page_size= \(pageSize)"]
+        }
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        return URLEncoding.httpBody
+    }
+    */
     
 }
